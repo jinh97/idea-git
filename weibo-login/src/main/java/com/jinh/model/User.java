240e5ddbd80640cc73a94a13c1c1dd945ea042bf
jinhuang <huangjin@irissz.com>
2021-01-08 16:58:02 +0800

    idea暂存区使用

--- weibo-login/src/main/java/com/jinh/model/User.java
+++ weibo-login/src/main/java/com/jinh/model/User.java
@@ -6,4 +6,45 @@ public class User {
     private String name;
     private String sex;
     private String phone;
+    private String address;
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
